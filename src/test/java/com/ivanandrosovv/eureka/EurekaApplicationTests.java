3a574ece892427c275be850ae09848a7672b23e4
dictator_zx <ivanandrosovv@gmail.com>
2023-07-02 19:17:44 +0300

    Hello

---
+++ src/test/java/com/ivanandrosovv/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanandrosovv.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
